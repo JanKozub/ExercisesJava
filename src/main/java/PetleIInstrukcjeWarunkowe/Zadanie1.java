f4b066198b16624265a5ea978dc691a0876d7acc
JanKozub <j.kozub136@gmail.com>
2018-07-23 21:37:59 +0200

    Wykonanie zadan do 11 w dziale o pętlach i instrukcjach warunkowych

---
+++ src/main/java/PetleIInstrukcjeWarunkowe/Zadanie1.java
@@ -0,0 +1,15 @@
+package PetleIInstrukcjeWarunkowe;
+
+public class Zadanie1 {
+
+    public static void odliczDoX(int liczba){
+
+        for (int i=0; i <= liczba; i++){
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        odliczDoX(5);
+    }
+}
---
+++ src/main/java/PetleIInstrukcjeWarunkowe/Zadanie6.java
@@ -0,0 +1,24 @@
+package PetleIInstrukcjeWarunkowe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Zadanie6 {
+    public static void podzielLiczbe(int liczba){
+        int licznik=0;
+        List<Integer> wynik = new ArrayList();
+        for (int i=0; Integer.toString(liczba).length() > wynik.size();i++){
+         if (Integer.toString(liczba).contains(Integer.toString(i))){
+             wynik.add(licznik,i);
+             licznik++;
+         }
+     }
+        for (int l=wynik.size()-1;l>=0;l--){
+            System.out.println(wynik.get(l));
+        }
+    }
+
+    public static void main(String[] args) {
+        podzielLiczbe(567);
+    }
+}
