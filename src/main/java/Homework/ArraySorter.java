71bab3a2f0e9f980d30c0a6fc2e24165d814194d
Jan <j.kozub136@gmail.com>
2019-02-23 23:23:00 +0100

    2nd method of sorting added

--- src/main/java/Homework/ArraySorter.java
+++ src/main/java/Homework/ArraySorter.java
@@ -3,12 +3,13 @@ package Homework;
 public class ArraySorter {
 
     public static void sort(int[] array) {
+        if (array == null)
+            return;
+
         int length = array.length;
         for (int g = 0; g < length; g++) {
-            for (int i = 0; i < array.length - 1; i++) {
-                if (array[i] < array[i + 1]) {
-
-                } else {
+            for (int i = 0; i < array.length - 1 - g; i++) {
+                if (array[i] >= array[i + 1]) {
                     int val = array[i];
                     array[i] = array[i + 1];
                     array[i + 1] = val;
@@ -17,21 +18,29 @@ public class ArraySorter {
         }
     }
 
-    public static void main(String[] args) {
-        int[] data = {-5,87,2,7,8,23,56,345,64,414,32,57,5};
-
-        for (int i = 0; i < data.length; i++) {
-            System.out.print(data[i]);
-            System.out.print(", ");
+    public static void sort2(int[] array, boolean kolejnosc) {
+        if (array == null)return;
+        int[] newArray = new int[array.length];
+        int val = array[0];
+        int num = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int g = 0; g < array.length; g++) {
+                if (val < array[g]) {
+                    val = array[g];
+                    num = g;
+                }
+            }
+            array[num] = 0;
+            newArray[i] = val;
+            val = 0;
         }
-        System.out.println("");
-        System.out.println("Sorted: ");
-
-        sort(data);
-
-        for (int i = 0; i < data.length; i++) {
-            System.out.print(data[i]);
-            System.out.print(", ");
+        if (kolejnosc) {
+            int size = newArray.length;
+            int[] array2 = new int[size];
+            for (int o = 0; o < newArray.length; o++) {
+                array2[o] = newArray[size - 1];
+                size--;
+            }
         }
     }
 }
