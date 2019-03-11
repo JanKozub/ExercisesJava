fb7a6389857d2843ee6fac5c72e67b8bce40c1a4
Jan <j.kozub136@gmail.com>
2019-03-11 21:48:54 +0100

    fixed sort2 method

--- src/main/java/Homework/ArraySorter.java
+++ src/main/java/Homework/ArraySorter.java
@@ -18,8 +18,8 @@ public class ArraySorter {
         }
     }
 
-    public static void sort2(int[] array, boolean kolejnosc) {
-        if (array == null)return;
+    public static int[] sort2(int[] array, boolean kolejnosc) {
+        if (array == null)return array;
         int[] newArray = new int[array.length];
         int val = array[0];
         int num = 0;
@@ -41,6 +41,44 @@ public class ArraySorter {
                 array2[o] = newArray[size - 1];
                 size--;
             }
+            return array2;
+        }
+        return newArray;
+    }
+
+    public static int[] sort3(int[] array, boolean kolejnosc) {
+        if (array == null)return array;
+        int[] newArray = new int[array.length];
+        int val = array[0];
+        int num = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int g = 0; g < array.length; g++) {
+                if (val < array[g]) {
+                    val = array[g];
+                    num = g;
+                }
+            }
+            array[num] = 0;
+            newArray[i] = val;
+            val = 0;
+        }
+        if (kolejnosc) {
+            int size = newArray.length;
+            int[] array2 = new int[size];
+            for (int o = 0; o < newArray.length; o++) {
+                array2[o] = newArray[size - 1];
+                size--;
+            }
+            return array2;
+        }
+        return newArray;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {45,7,23,26,36,6};
+        array = sort3(array, true);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
         }
     }
 }
