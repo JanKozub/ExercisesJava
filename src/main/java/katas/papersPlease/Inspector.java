a48be249ca16c98b6df276820e9252817e1c5029
Jan <j.kozub136@gmail.com>
2021-06-29 17:38:52 +0200

    Reorganization of files plus new kata

--- src/main/java/katas/papersPlease/Inspector.java
+++ src/main/java/katas/papersPlease/Inspector.java
@@ -1,16 +1,37 @@
 package katas.papersPlease;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Map;
 
 public class Inspector {
-    private ArrayList<String> allowedCitizens = new ArrayList<>();
-    private ArrayList<String> deniedCitizens = new ArrayList<>();
+    private String[] allowedCitizens;
+    private String[] deniedCitizens;
+
+    private String[] wantedCriminals;
+
+    private boolean entrantsRequirePassport = false;
 
     public void receiveBulletin(String bulletin) {
         String[] infos = bulletin.split("\n");
         System.out.println(Arrays.toString(infos));
+
+        for (String info : infos) {
+            if (info.contains("require passport")) {
+                if (info.contains("Entrants"))
+                    entrantsRequirePassport = true;
+            } else {
+                if (info.contains("Allow citizens")) {
+                    allowedCitizens = info.split("of ")[1].split(", ");
+                } else {
+                    if (info.contains("Deny citizens")) {
+                        deniedCitizens = info.split("of ")[1].split(", ");
+                    } else {
+                        wantedCriminals = info.split(": ")[1].split(", ");
+                    }
+                }
+            }
+        }
+        System.out.println(Arrays.toString(wantedCriminals));
     }
 
     public String inspect(Map<String, String> person) {
--- src/main/java/katas/papersPlease/Papers.java
+++ src/main/java/katas/papersPlease/Papers.java
@@ -4,6 +4,6 @@ public class Papers {
     public static void main(String[] args) {
         Inspector inspector = new Inspector();
 
-        inspector.receiveBulletin("Entrants require passport\nAllow citizens of Arstotzka, Obristan");
+        inspector.receiveBulletin("Entrants require passport\nAllow citizens of Arstotzka, Obristan\nWanted by the State: Hubert Popovic");
     }
 }
